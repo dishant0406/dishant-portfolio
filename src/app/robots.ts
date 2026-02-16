25fdac96cdafc35ec5968f6507f0e87addddb9ce
Dishant <dishu5570@gmail.com>
2026-02-16 21:14:36 +0530

    feat: add robots.txt generation for SEO optimization

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from 'next'
+import { env } from '../lib/env'
+
+export default function robots(): MetadataRoute.Robots {
+  const siteUrl = (env.NEXT_PUBLIC_SITE_URL || '').replace(/\/$/, '')
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
+  }
+}
